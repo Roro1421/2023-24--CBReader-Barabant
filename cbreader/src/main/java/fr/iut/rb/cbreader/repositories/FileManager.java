2f17d607a1b4eb376b18fbe8f18e275ebd339807
Roro1421 <romain.barabant@iut-dijon.u-bourgogne.fr>
2024-01-30 17:03:42 +0100

    TP2.4

--- cbreader/src/main/java/fr/iut/rb/cbreader/repositories/FileManager.java
+++ cbreader/src/main/java/fr/iut/rb/cbreader/repositories/FileManager.java
@@ -1,5 +1,41 @@
 package fr.iut.rb.cbreader.repositories;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public class FileManager {
-    
+    private String booksLocation;
+
+    /**
+     * @param booksLocation
+     */
+    public FileManager(String booksLocation) {
+        this.booksLocation = booksLocation;
+    }
+
+    /**
+     * store a file send by form
+     * 
+     * @param file     the file to store
+     * @param fileName the name of the file (simple filename, no path)
+     * @throws IOException if the file can not be written
+     */
+    public void uploadFile(MultipartFile file, String fileName) throws IOException {
+        if (fileName == "" || fileName == null) {
+            fileName = file.getOriginalFilename();
+        }
+        Path destinationPath = Paths.get(booksLocation, fileName);
+        destinationPath = destinationPath.normalize().toAbsolutePath();
+        try (InputStream stream = file.getInputStream()) {
+            Files.copy(stream, destinationPath,
+                    StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
 }
--- cbreader/src/main/java/fr/iut/rb/cbreader/services/ComicsService.java
+++ cbreader/src/main/java/fr/iut/rb/cbreader/services/ComicsService.java
@@ -1,8 +1,38 @@
 package fr.iut.rb.cbreader.services;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import fr.iut.rb.cbreader.models.ComicBook;
+import fr.iut.rb.cbreader.models.ComicBookData;
+import fr.iut.rb.cbreader.repositories.FileManager;
+import fr.iut.rb.cbreader.repositories.IComicsRepository;
 
 @Service
 public class ComicsService {
-    
+    public IComicsRepository repository;
+    public FileManager fileUploader;
+
+    /**
+     * @param repository
+     * @param filePath
+     */
+    public ComicsService(IComicsRepository repository, @Value("${filePath}") String filePath) {
+        this.repository = repository;
+        this.fileUploader = new FileManager(filePath);
+    }
+
+    /**
+     * @param book
+     * @param file
+     * @throws IOException
+     */
+    public void AddComicBook(ComicBook book, MultipartFile file) throws IOException{
+        fileUploader.uploadFile(file, book.getFileName());
+        ComicBookData comicBookData = new ComicBookData(book);
+        repository.insert(comicBookData);
+    }
 }
