2065c8e6bf9823d2e0bfaff3e7a50b91547c0ef1
Roro1421 <romain.barabant@iut-dijon.u-bourgogne.fr>
2024-01-30 09:14:55 +0100

    Init

---
+++ cbreader/src/main/java/fr/iut/rb/cbreader/CbreaderApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.rb.cbreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbreaderApplication.class, args);
+	}
+
+}
---
+++ cbreader/src/test/java/fr/iut/rb/cbreader/CbreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.rb.cbreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
